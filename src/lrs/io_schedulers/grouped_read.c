87432056c5511eec3e09d2f03d660d00b5a0ba94
yoann <yoann@linux3.home>
2026-08-31 19:16:53 +0200

    fix: ensure medium is only released when necessary
    In grouped_read. If the medium is released too early, and we try again
    to use it, we may end up in a situation where we release the medium too
    many times, resulting in a segfault after a few retries. To prevent this
    issue, instead of releasing early and only if a few cases, release the
    medium before it is unset.
    
    For a more in-depth analysis of the fix, check out:
    https://github.com/phobos-storage/phobos/issues/68
    
    Signed-off-by: YoannV-Ayudata <yoann.valeri.externe@ayudata.fr>

--- src/lrs/io_schedulers/grouped_read.c
+++ src/lrs/io_schedulers/grouped_read.c
@@ -1361,19 +1361,13 @@ static int grouped_retry(struct io_scheduler *io_sched,
          * medium or the one that was just tried if no error occured on the
          * medium.
          */
-        if ((*medium)->health == 0) {
-            /* release the previous reference in the request container */
-            lrs_medium_release(*medium);
+        if ((*medium)->health == 0)
             sreq->medium_index = reqc->req->ralloc->n_required;
-        }
+
         /* else: we use sreq->medium_index i.e. the index of the medium that we
          * failed to load.
          */
     } else {
-        if (pho_id_equal(&queue_to_use->medium_id, &(*medium)->rsc.id))
-            /* release the previous reference in the request container */
-            lrs_medium_release(*medium);
-
         sreq->medium_index =
             read_req_get_medium_index(reqc, &queue_to_use->medium_id);
     }
@@ -1401,6 +1395,7 @@ static int grouped_retry(struct io_scheduler *io_sched,
         }
     }
 
+    lrs_medium_release(*medium);
     /* On error, always fetch DSS information since the caller doesn't know if
      * the \p medium was just allocated or not. It cannot free it.
      */
